72b99223b50029eb4b36507e12be0c99700cbdb3
Zyro9922 <alihasan9922@gmail.com>
2023-10-21 14:35:24 +0530

    Dagger is working as expected

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.mydaggerexample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mydaggerexample"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -38,7 +38,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -47,7 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Dependency injection
-    implementation("com.google.dagger:dagger:2.42")
-    kapt("com.google.dagger:dagger-compiler:2.42")
+    implementation("com.google.dagger:dagger:2.47")
+    kapt("com.google.dagger:dagger-compiler:2.47")
 
 }
--- app/src/main/java/com/example/mydaggerexample/MainActivity.kt
+++ app/src/main/java/com/example/mydaggerexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mydaggerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mydaggerexample.DaggerUserRegistrationComponent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         val userRepository: UserRepository
         val emailService: EmailService
 
+        val daggerUserRegistrationComponent = DaggerUserRegistrationComponent.create()
+        daggerUserRegistrationComponent.getUserRegistrationService().registerUser("something@gmail.com", "mypassword")
 
     }
 }
